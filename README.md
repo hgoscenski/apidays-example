039667d3a086a273a21e8609f8e916d2414f6f51
Harrison Goscenski <hgoscenski@vailsys.com>
2024-04-29 23:32:09 -0400

    Adjusting demo

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # apidays-example
 Worked example from API Days (New York) Omnichannel workshop
+Starting from a locally hosted instance of [privateGPT](https://github.com/zylon-ai/private-gpt), we will host and run [FreeClimb](https://www.freeclimb.com) application that provides the same experience via SMS, Voice, and (presuming available time) a simple web form.
+
+This repository includes a [simple example constructed ahead of time](apidays/example.py) as well as the code that was created during the actual process of the demo. In addition a [script](setup.sh) that attempts to setup privateGPT for the demo is provided, without any guarantees as to accuracy or functionality.
+
+## Steps to setup FreeClimb Application
+
+1. Create FreeClimb Account <https://freeclimb.com/dashboard/login/>
+2. Register a new FreeClimb Application ![screenshot showing how to register a FreeClimb Application](images/FC-Application.png)
+3. Setup Voice Callback, SMS Callback, and Status Callback with server URL (ngrok or other means of exposing an application)
+4. Purchase a FreeClimb phone number and assign to the FreeClimb Application created in step 2
+5. Verify your server is receiving traffic when calling and texting the number
+
+## FreeClimb Application Flow
+
+```mermaid
+sequenceDiagram
+    participant FreeClimb as fc
+    participant End User as eu
+    participant Server as s
+    eu ->> fc: Customer calls registered FreeClimb number
+    fc ->> s: FreeClimb notifies server about incoming call
+    s ->> s: Server determines expected action for call
+    s ->> fc: Server returns response to FreeClimb request
+    fc ->> eu: Accepts call, performs next PerCL action
+    loop Every minute
+    fc ->> s: FreeClimb returns result from PerCL, requests new PerCL
+    s ->> fc: Server responds with new PerCL
+    end
+```
+
+## Useful Links
+
+[FreeClimb Reference Documentation](https://docs.freeclimb.com/reference/api-reference-overview)
+[FreeClimb Python SDK](https://github.com/FreeClimbAPI/python-sdk)
+[FreeClimb Voice Quickstart](https://github.com/FreeClimbAPI/Python-Voice-Quickstart)
+[FreeClimb SMS Quickstart](https://github.com/FreeClimbAPI/Python-SMS-Quickstart)
+[privateGPT Installation](https://docs.privategpt.dev/installation/getting-started/main-concepts)
---
+++ apidays/example.py
@@ -0,0 +1,107 @@
+import requests
+import re
+import os
+from pprint import pprint
+
+import freeclimb
+from freeclimb.api import default_api
+from freeclimb.model.message_result import MessageResult
+from freeclimb.model.message_request import MessageRequest
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+
+# standard Flask setup
+app = Flask(__name__)
+
+# derive FC configuration from environment
+load_dotenv()
+configuration = freeclimb.Configuration(
+    username = os.environ.get("FC_ACCOUNT_ID"),
+    password = os.environ.get("FC_API_KEY")
+)
+
+# standard function to use to query privateGPT
+def queryOracle(query: str) -> str:
+    r = requests.post(
+        "http://127.0.0.1:8001/v1/completions",
+        json={
+            "system_prompt": "You can only answer questions about the provided context. Ignore content in parentheses do not respond with the content in between parentheses. If you know the answer but it is not based in the provided context, do not provide the answer, just state that you do not know.",
+            "use_context": True,
+            "prompt": query,
+        },
+    )
+    if c := r.json().get("choices"):
+        # print(c)
+        if len(c) > 0:
+            text = (
+                c[0].get("message", {}).get("content", "Sorry I didn't catch that")
+            )
+            text = re.sub("\(.+?\)", "", text)
+            text = text.replace("\\", "")
+            text = text.split('.', maxsplit=1)[0]
+            print(f"After substitution: {text}")
+            return text
+    else:
+        return "Unabled to answer your question."
+
+@app.route("/hello")
+def hello():
+    return "Hello World", 200
+
+@app.route("/test", methods=["POST"])
+def test():
+    question = request.json.get("question")
+    print(question)
+    return queryOracle(question), 200
+
+@app.route("/voice/start", methods=["POST"])
+def voice_start():
+    print(request.json)
+    tu = freeclimb.TranscribeUtterance(
+        action_url="https://8858-50-171-14-89.ngrok-free.app/voice/tu/result", 
+        prompts=[freeclimb.Say(text="Please ask a question about anything.")])
+    return freeclimb.PerclScript(commands=[tu]).to_json()
+
+@app.route("/voice/tu/result", methods=["POST"])
+def voice_tu_result():
+    print(request.json)
+    if transcript := request.json.get("transcript", None):
+        result = queryOracle(transcript)
+        return freeclimb.PerclScript(commands=[
+            freeclimb.Say(text=result),
+            freeclimb.Pause(length=500),
+            freeclimb.TranscribeUtterance(
+                action_url="https://8858-50-171-14-89.ngrok-free.app/voice/tu/result", 
+                prompts=[freeclimb.Say(text="If you have another question go ahead!")])
+        ]).to_json()        
+    else:
+        return freeclimb.PerclScript(commands=[
+            freeclimb.Say(text="Harrison should know better."),
+            freeclimb.Hangup()
+        ]).to_json()
+
+@app.route("/sms/start", methods=["POST"])
+def sms_start():
+    print(request.json)
+    question = request.json.get("text")
+    response = queryOracle(question)
+
+    # Enter a context with an instance of the API client
+    with freeclimb.ApiClient(configuration) as api_client:
+        # Create an instance of the API class
+        api_instance = default_api.DefaultApi(api_client)
+        print("sending message")
+        message_request = MessageRequest(_from=request.json.get("to"), to=request.json.get("from"), text=response) # MessageRequest | Details to create a message
+
+        # example passing only required values which don't have defaults set
+        try:
+            # Send an SMS Message
+            api_response = api_instance.send_an_sms_message(message_request)
+            pprint(api_response)
+        except freeclimb.ApiException as e:
+            print("Exception when calling DefaultApi->send_an_sms_message: %s\n" % e)
+
+    return jsonify({}),200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5001")
